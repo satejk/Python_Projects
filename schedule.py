b5865c6c8ac03eca8d4a0d58c83e874b1f6ce310
Satej Khandeparkar <satejk5@gmail.com>
2015-07-26 15:01:03 +0200

    rate monotonic scheduler
    Simulation tool for the rate monotonic algorithm for real time systems.
    Released under the GNU public License.

--- schedule.py
+++ schedule.py
@@ -1,4 +1,27 @@
 __author__ = 'satej'
+/*
+ * This file is part of my Distributed Real Time Systems LAB.
+ *
+ * 
+ *
+ * Copyright (C) 2015 satej v. khandeparkar <satejk5@gmail.com> , Halmstad University.
+ * 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.
+ *
+ * be warned, this file is to used for education purpose only
+ */
 
 
 
